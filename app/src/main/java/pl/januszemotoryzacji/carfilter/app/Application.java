4143aee19c9ae66dcbe9b4c0e36da01f79113071
lukasz.barc <barclukasz@gmail.com>
2016-03-18 19:29:48 +0100

    application class

---
+++ app/src/main/java/pl/januszemotoryzacji/carfilter/app/Application.java
@@ -0,0 +1,12 @@
+package pl.januszemotoryzacji.carfilter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
