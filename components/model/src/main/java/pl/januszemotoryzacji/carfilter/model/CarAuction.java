17577a614caabd9ee04511d908bd5ed6a2764ec0
lukasz.barc <barclukasz@gmail.com>
2016-03-18 21:12:20 +0100

    model

---
+++ components/model/src/main/java/pl/januszemotoryzacji/carfilter/model/CarAuction.java
@@ -0,0 +1,24 @@
+package pl.januszemotoryzacji.carfilter.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class CarAuction {
+
+    private long identity;
+    private int version;
+    private String make;
+    private String model;
+    private String model2;
+    private int yearOfProduction;
+    private int power;
+    private FuelType fuelType;
+    private String province;
+    private int mileage;
+    private BigDecimal price;
+    private Date endDate;
+
+}
---
+++ components/model/src/main/java/pl/januszemotoryzacji/carfilter/model/FuelType.java
@@ -0,0 +1,7 @@
+package pl.januszemotoryzacji.carfilter.model;
+
+
+public enum FuelType {
+
+    BENZIN, DIESEL, LPG;
+}
